0ccd92c6ea200a2bd04067c882752073f7c01535
maknoon1a <ahmad.omar@ui.ac.id>
2026-04-29 17:59:05 +0700

    membuat context processor untuk mengakses role, menjadikan navbar menggunakan query parameter untuk mengidentifikasi jenis role

--- tk03_basdat/settings.py
+++ tk03_basdat/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'tk03_basdat.context_processors.role',
             ],
         },
     },
