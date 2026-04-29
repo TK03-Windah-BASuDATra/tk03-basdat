7eb0afb9ec526ec960f6913fd71f8094f3a86eaf
maknoon1a <ahmad.omar@ui.ac.id>
2026-04-29 15:56:53 +0700

    fitur CRUD tiket kategori v0

--- tk03_basdat/urls.py
+++ tk03_basdat/urls.py
@@ -6,31 +6,31 @@ urlpatterns = [
     path('artist/', include('artist.urls')),
     path('ticket-category/', include('ticket_category.urls')),
 ]
-from . import views
+# from . import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.dashboard, name="dashboard"),
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("", views.dashboard, name="dashboard"),
     
-    path("accounts/", include("accounts.urls")),
-    path('events/', include('events.urls')),
+#     path("accounts/", include("accounts.urls")),
+#     path('events/', include('events.urls')),
 
-    path("profile/", views.profile, name="profile"),
+#     path("profile/", views.profile, name="profile"),
 
-    path("manajemen-venue/", views.manajemen_venue, name="manajemen_venue"),
-    path("manajemen-kursi/", views.manajemen_kursi, name="manajemen_kursi"),
-    path("kategori-tiket/", views.kategori_tiket, name="kategori_tiket"),
-    path("manajemen-tiket/", views.manajemen_tiket, name="manajemen_tiket"),
-    path("semua-order/", views.semua_order, name="semua_order"),
-    path("tiket-aset/", views.tiket_aset, name="tiket_aset"),
-    path("order-aset/", views.order_aset, name="order_aset"),
+#     path("manajemen-venue/", views.manajemen_venue, name="manajemen_venue"),
+#     path("manajemen-kursi/", views.manajemen_kursi, name="manajemen_kursi"),
+#     path("kategori-tiket/", views.kategori_tiket, name="kategori_tiket"),
+#     path("manajemen-tiket/", views.manajemen_tiket, name="manajemen_tiket"),
+#     path("semua-order/", views.semua_order, name="semua_order"),
+#     path("tiket-aset/", views.tiket_aset, name="tiket_aset"),
+#     path("order-aset/", views.order_aset, name="order_aset"),
 
-    path("event-saya/", views.event_saya, name="event_saya"),
+#     path("event-saya/", views.event_saya, name="event_saya"),
 
-    path("tiket-saya/", views.tiket_saya, name="tiket_saya"),
-    path("pesanan/", views.pesanan, name="pesanan"),
-    path("cari-event/", views.cari_event, name="cari_event"),
-    path("promosi/", views.promosi, name="promosi"),
-    path("venue/", views.venue, name="venue"),
-    path("artis/", views.artis, name="artis"),
-]
+#     path("tiket-saya/", views.tiket_saya, name="tiket_saya"),
+#     path("pesanan/", views.pesanan, name="pesanan"),
+#     path("cari-event/", views.cari_event, name="cari_event"),
+#     path("promosi/", views.promosi, name="promosi"),
+#     path("venue/", views.venue, name="venue"),
+#     path("artis/", views.artis, name="artis"),
+# ]
