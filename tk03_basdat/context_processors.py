a1587e4b8992071b41c95c7ab22ef5c905319954
maknoon1a <ahmad.omar@ui.ac.id>
2026-04-29 18:18:59 +0700

    tambahan minor

--- tk03_basdat/context_processors.py
+++ tk03_basdat/context_processors.py
@@ -1,5 +1,13 @@
 def role(request):
     r = request.GET.get('role')
     if r not in ('admin', 'organizer', 'customer'):
-        r = None  # guest / belum pilih role
+        r = None  # guest (belum pilih role)
     return {'role': r}
+
+# buat nanti kalo udah implementasi pake sql dan udah nyimpen role di session
+# def role(request):
+#     r = request.session.get('role')
+#     if r not in ('admin', 'organizer', 'customer'):
+#         r = None
+#     return {'role': r}
+
