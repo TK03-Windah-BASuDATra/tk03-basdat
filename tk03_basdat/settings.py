77066548545d6531004ee4277f2d0da7474d7c9c
AnggaTriSetiawan <anggatrisetiawan81@gmail.com>
2026-04-29 13:51:17 +0700

    Base.html -> (Base.html & navbar.html) dan fix urls level project

--- tk03_basdat/settings.py
+++ tk03_basdat/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'artist',
-    'ticket_category'
+    'ticket_category',
     'accounts',
     'events',
 ]
