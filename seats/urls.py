5e6de9ff2ec878d4ddea0f712fd2862fc53ca322
daffa i <daffa2.ismail178@gmail.com>
2026-04-30 14:07:33 +0700

    fix: adding ticket and seats namespace and prepending appname: in navbar

--- seats/urls.py
+++ seats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from django.urls import path
 from . import views
+
+app_name = 'seats'
 
 urlpatterns = [
     path('', views.manajemen_kursi, name='manajemen_kursi'),
--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'tickets'
+
 urlpatterns = [
     path('', views.manajemen_tiket, name='manajemen_tiket'),
 ]
